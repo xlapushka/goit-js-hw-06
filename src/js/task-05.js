f391274fa9c1320ad6b5a2954d2fedb2c33f6ff7
Yuliia Hubarenko <xlapushka@ukr.net>
2023-05-04 01:51:55 +0300

    in task 5 added trim()
    прибирає зайві пробіли

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,7 +2,7 @@ const currentName = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 currentName.addEventListener("input", (event) => {
-  event.currentTarget.value === ""
+  event.currentTarget.value.trim() === ""
     ? (output.textContent = "Anonymous")
     : (output.textContent = event.currentTarget.value);
 });
