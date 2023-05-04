be30bcf578c8af06d3cc2f88454750782052dd7a
Yuliia Hubarenko <xlapushka@ukr.net>
2023-05-04 13:36:49 +0300

    task 8 is done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,34 @@
+const form = document.querySelector(".login-form");
 
+function pickUpInForm(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value.trim() === "") {
+    alert("🙏 Please enter all data!! 😺🐭");
+  }
+
+  if (password.value.trim() === "") {
+    alert("🙏 Please enter all data!! 😺🐭");
+  }
+
+  userInfo(email.value.trim(), password.value.trim());
+}
+
+function userInfo(email, password) {
+  let userInfo = new Object();
+
+  userInfo.email = email;
+  userInfo.password = password;
+
+  console.log(userInfo);
+  clearForm();
+}
+
+function clearForm() {
+  form.reset();
+}
+
+form.addEventListener("submit", pickUpInForm);
