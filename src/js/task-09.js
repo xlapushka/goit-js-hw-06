cffceeed1165544ec694f4448166d99d768dbad7
Yuliia Hubarenko <xlapushka@ukr.net>
2023-05-06 03:22:53 +0300

    task 9 is done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,23 @@
+const changeColor = document.querySelector(".change-color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function changeBackgroundColor() {
+  const widget = document.querySelector(".widget");
+  const body = widget.parentNode;
+
+  body.setAttribute("style", `background-color: ${getRandomHexColor()}`);
+  changeSpan();
+}
+
+function changeSpan() {
+  const span = document.querySelector(".color");
+
+  span.textContent = `- ${getRandomHexColor()}`;
+}
+
+changeColor.addEventListener("click", changeBackgroundColor);
